8504232cdd7bc58896dcc9181f1f721fb41affdd
m-xtnd <m_extended@tutanota.com>
2023-12-19 19:23:17 +0100

    Exercice 13

---
+++ exercices_java/src/exercice_13/App.java
@@ -0,0 +1,35 @@
+package exercice_13;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Écrivez un programme Java pour afficher un visage.
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println("Voulez-vous voir le visage de Damien ? Tapez 'oui' ou 'non !'");
+            String choix = scanner.nextLine();
+
+            switch (choix) {
+                case "oui":
+                    System.out.println("+''''''+");
+                    System.out.println("[| o o |]");
+                    System.out.println("|   ^   |");
+                    System.out.println("|  '-'  |");
+                    System.out.println(" +-----+");
+                    return;
+                case "non":
+                    System.out.println("Vous ratez quelque chose...");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Il fallait mieux lire la consigne, on recommence !");
+            }
+
+        }
+
+    }
+}
